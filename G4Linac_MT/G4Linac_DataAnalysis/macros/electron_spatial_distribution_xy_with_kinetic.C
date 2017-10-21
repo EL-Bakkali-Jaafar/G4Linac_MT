98cac77d450f47c5f18c5075555e0e1c956c2078
Jaafar EL Bakkali <bahmedj@gmail.com>
2017-10-21 18:13:07 +0100

    Delete electron_spatial_distribution_xy_with_kinetic.C

--- G4Linac_MT/G4Linac_DataAnalysis/macros/electron_spatial_distribution_xy_with_kinetic.C
+++
@@ -1,8 +0,0 @@
-{
-gROOT -> Reset();
-TFile f("Data.root");
-TCanvas* Canvas_electron_spatial_distribution_xy_with_kinetic = new TCanvas("Canvas_electron_spatial_distribution_xy_with_kinetic", " ");
-electron_spatial_distribution_xy_with_kinetic->GetXaxis()->SetTitle(" x (cm)");
-electron_spatial_distribution_xy_with_kinetic->GetYaxis()->SetTitle(" y (cm)");
-electron_spatial_distribution_xy_with_kinetic->Draw("colz");
-}
